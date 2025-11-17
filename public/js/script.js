abddcc27cbf27222a30967f6d5988b706dfa8aa1
anne-mart <mart1095@stthomas.edu>
2025-11-17 09:13:56 -0600

    More moving of files + getting things to run. Will update rest_server.mjs in next commit -- WIP

--- public/js/script.js
+++ public/js/script.js
@@ -1,67 +1,27 @@
-// TODO :: Update this entire file to add functionality to the HTML page
+// TODO :: this should be broken up into individual REST calls, have it like this for now to test fetch from client side
 
 console.log('Script is running');
 
-// add event listener for button
-document.getElementById('add-btn').addEventListener('click', (event) => {
-    console.log('Submit Pressed')
-    let data = {
-        id: parseInt(document.getElementById('id_input').value),
-        name: document.getElementById('name_input').value,
-        email: document.getElementById('email_input').value
-    }
-    let options = {
-        method: 'PUT',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(data)
-    };
+fetch("/incidents")
+    .then(res => res.json())
+    .then(data => {
+        const tbody = document.getElementById("incident-tbody");
+        data.forEach(inc => {
+            let row = document.createElement("tr");
 
-    fetch('/add-user', options)
-    .then((response) => {
-        return response.text();
+            row.innerHTML = `
+                <td>${inc.case_number}</td>
+                <td>${inc.date}</td>
+                <td>${inc.time}</td>
+                <td>${inc.code}</td>
+                <td>${inc.incident}</td>
+                <td>${inc.police_grid}</td>
+                <td>${inc.neighborhood_number}</td>
+                <td>${inc.block}</td>
+            `;
+            tbody.appendChild(row);
+        });
     })
-    .then((data) => {
-        console.log(data);
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-}, false);
-
-
-fetch('/list-users', {method: 'GET'})
-.then((response) => {
-    return response.json();
-})
-.then((data) => {
-    console.log(data);
-    // use the data to add table rows to table body
-    // this will run automatically when you go to browser and port 8080
-
-    // add user to table
-    let tableBody = document.getElementById('user-tbody');
-    data.users.forEach((user) => {
-        let row = document.createElement('tr');
-
-        // ID cell
-        let idCell = document.createElement('td');
-        idCell.textContent = user.id;
-        row.appendChild(idCell);
-
-        // Name cell
-        let nameCell = document.createElement('td');
-        nameCell.textContent = user.name;
-        row.appendChild(nameCell);
-
-        // Email cell
-        let emailCell = document.createElement('td');
-        emailCell.textContent = user.email;
-        row.appendChild(emailCell);
-
-        tableBody.appendChild(row);
-    });
-})
-.catch((err) => {
-    console.log(err);
-});
-
+    // .catch(err => {
+    //     console.log(err);
+    // });
